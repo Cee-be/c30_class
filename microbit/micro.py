27ce7b474a00fc8fc746905683792d6f38bfad65
Cee-be <7015733@spsd.sk.ca>
2025-10-14 14:13:53 -0600

    playing with arduino(old) and microbit(new)

---
+++ microbit/micro.py
@@ -0,0 +1,12 @@
+# Project Title
+# Your Name
+# Date
+#
+# Extra for Experts:
+# - describe what you did to take this project "above and beyond"
+
+
+import micro
+while True:
+    display.scroll("Hello")
+    sleep(1000)
---
+++ test-arduino/sketch.js
@@ -0,0 +1,24 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+const { Board, Led } = require("johnny-five");
+const board = new Board({ port: "COM3", repl: false });
+
+board.on("ready", function() {
+  console.log("✅ Board connected and ready!");
+  
+  const led = new Led(13); // use the built-in LED (pin 13)
+  led.blink(500); // blink every 0.5 seconds
+
+  // When you stop Node (Ctrl + C), turn off the LED safely
+  process.on("SIGINT", function() {
+    led.stop().off();
+    console.log("\n🟡 LED stopped and turned off. Goodbye!");
+    process.exit();
+  });
+});
