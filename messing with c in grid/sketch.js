0cf78ebbb43c7ed093c6f5ccc06dba0da1cb59fe
Cee-be <7015733@spsd.sk.ca>
2025-11-06 09:34:54 -0600

    including images and setting the background

--- messing with c in grid/sketch.js
+++ messing with c in grid/sketch.js
@@ -1,12 +1,14 @@
 // 
 //
 //
+//images - https://github.com/hadigghazi/FireBoy-and-WaterGirl/commits?author=DawoudTormos
 
-const CELL_SIZE = 200;
+const CELL_SIZE = 100;
 const OPEN_TILE = 0;
 const IMPOSSIBLE = 1;
-//const WATER = 0;
-//const LAVA = 0;
+const WATER = 3;
+const LAVA = 2;
+const DOOR = 4;
 const PLAYER = 9;
 const PLAYER2 = 8;
 let grid;
@@ -46,19 +48,23 @@ function draw() {
 function preload(){
   playerImg1 = loadImage("fireboy.png");
   playerImg2 = loadImage("watergirl.png");
+
+  gameBackground = loadImage("Ground.png");
+
+  door = loadImage("door.png");
 }
 
-function toggleCell(x, y) {
-  //make sure the cell you're toggling actually exists!
-  if (x >= 0 && x < cols && y >= 0 && y < rows) {
-    if (grid[y][x] === OPEN_TILE) {
-      grid[y][x] = IMPOSSIBLE;
-    }
-    else if (grid[y][x] === IMPOSSIBLE) {
-      grid[y][x] = OPEN_TILE;
-    }
-  }
-}
+// function toggleCell(x, y) {
+//   //make sure the cell you're toggling actually exists!
+//   if (x >= 0 && x < cols && y >= 0 && y < rows) {
+//     if (grid[y][x] === OPEN_TILE) {
+//       grid[y][x] = IMPOSSIBLE;
+//     }
+//     else if (grid[y][x] === IMPOSSIBLE) {
+//       grid[y][x] = OPEN_TILE;
+//     }
+//   }
+// }
 
 function keyPressed() {
   //fireboy movement
@@ -134,6 +140,7 @@ function movePlayer2(player2, newX, newY){
 }
 
 function displayGrid() {
+  image (gameBackground, 0, 0, width, height);
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
       let cell = grid[y][x];
@@ -146,24 +153,22 @@ function displayGrid() {
       else if (cell === IMPOSSIBLE){
         fill("black");
       }
-      else if (cell === 2){
+      else if (cell === LAVA){
         fill("red");
       }
-      else if (cell === 3){
+      else if (cell === WATER){
         fill("blue");
       }
-      else if (cell === 4){
+      else if (cell === DOOR){
         fill("green");
       }
       rect(xpos, ypos, CELL_SIZE, CELL_SIZE);
 
       if (cell === PLAYER){
         image(playerImg1, xpos, ypos, CELL_SIZE, CELL_SIZE);
-        // fill("red");
       }
       else if (cell === PLAYER2){
         image(playerImg2, xpos, ypos, CELL_SIZE, CELL_SIZE);
-        // fill("red");
       }
     }
   }
@@ -177,14 +182,14 @@ function generateRandomGrid(cols, rows) {
       let r = random(1);
       //pick 0 or 1 randomly
       if (random(100) < 50) {
-        newGrid[y].push(OPEN_TILE);
+        grid[y].push(OPEN_TILE);
       }
       else {
-        newGrid[y].push(IMPOSSIBLE);
+        grid[y].push(IMPOSSIBLE);
       }
     }
   }
-  return newGrid;
+  return grid;
 }
 
 function generateEmptyGrid(cols, rows) {
