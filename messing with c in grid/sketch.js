d555007e493b53a1eb4032979694e7a0ff38542b
Cee-be <7015733@spsd.sk.ca>
2025-11-06 08:50:14 -0600

    chanegs to image size

--- messing with c in grid/sketch.js
+++ messing with c in grid/sketch.js
@@ -1,8 +1,12 @@
-// Rectangle Neighbours 2d Array Demo
+// 
+//
+//
 
 const CELL_SIZE = 200;
 const OPEN_TILE = 0;
 const IMPOSSIBLE = 1;
+//const WATER = 0;
+//const LAVA = 0;
 const PLAYER = 9;
 const PLAYER2 = 8;
 let grid;
@@ -10,7 +14,7 @@ let rows;
 let cols;
 let thePlayer = {
   x: 0,
-  y: 0,
+  y: 3,
 };
 let thePlayer2 = {
   x: 0,
@@ -28,6 +32,10 @@ function setup() {
   //add player to grid
   grid[thePlayer.y][thePlayer.x] = PLAYER;
   grid[thePlayer2.y][thePlayer2.x] = PLAYER2;
+
+  //resizing image
+  playerImg1.resize(300, 500);
+  playerImg2.resize(300, 500);
 }
 
 function draw() {
@@ -39,20 +47,6 @@ function preload(){
   playerImg1 = loadImage("fireboy.png");
   playerImg2 = loadImage("watergirl.png");
 }
-
-// function mousePressed() {
-//   let x = Math.floor(mouseX/CELL_SIZE);
-//   let y = Math.floor(mouseY/CELL_SIZE);
-
-//   //self
-//   toggleCell(x ,y);
-
-//   //neighbours
-//   toggleCell(x + 1, y);
-//   toggleCell(x - 1, y);
-//   toggleCell(x, y - 1);
-//   toggleCell(x, y + 1);
-// }
 
 function toggleCell(x, y) {
   //make sure the cell you're toggling actually exists!
@@ -71,9 +65,6 @@ function keyPressed() {
   if (key === "w"){
     movePlayer(thePlayer, thePlayer.x, thePlayer.y - 1);
   }
-  else if (key === "s"){
-    movePlayer(thePlayer, thePlayer.x, thePlayer.y + 1);
-  }
   else if (key === "d"){
     movePlayer(thePlayer, thePlayer.x + 1, thePlayer.y);
   }
@@ -82,9 +73,18 @@ function keyPressed() {
   }
 
   //watergirl movement
-  
+  if (keyCode === UP_ARROW){
+    movePlayer2(thePlayer2, thePlayer2.x, thePlayer2.y - 1);
+  }
+  else if (keyCode ===RIGHT_ARROW){
+    movePlayer2(thePlayer2, thePlayer2.x + 1, thePlayer2.y);
+  }
+  else if (keyCode === LEFT_ARROW){
+    movePlayer2(thePlayer2, thePlayer2.x - 1, thePlayer2.y);
+  }
 }
 
+//moving player1(fireboy)
 function movePlayer(player, newX, newY){
   if (newX < 0 || newX >= cols || newY < 0 || newY >= rows){
     return;
@@ -96,6 +96,7 @@ function movePlayer(player, newX, newY){
     //moving player locat
     thePlayer.x = newX;
     thePlayer.y = newY;
+
   
     //mark new spot
     if (player === thePlayer){
@@ -103,6 +104,31 @@ function movePlayer(player, newX, newY){
     } 
     else {
       grid[player.y][player.x] = PLAYER2;
+    }
+  }
+}
+
+
+//moving player2(watergirl)
+function movePlayer2(player2, newX, newY){
+  if (newX < 0 || newX >= cols || newY < 0 || newY >= rows){
+    return;
+  }
+
+  if (grid[newY][newX] === OPEN_TILE) {
+    grid[player2.y][player2.x] = OPEN_TILE;
+
+    //moving player locat
+    thePlayer2.x = newX;
+    thePlayer2.y = newY;
+
+  
+    //mark new spot
+    if (player2 === thePlayer2){
+      grid[player2.y][player2.x] = PLAYER2;
+    } 
+    else {
+      grid[player.y][player.x] = PLAYER;
     }
   }
 }
@@ -120,11 +146,9 @@ function displayGrid() {
       }
       else if (grid[y][x] === PLAYER){
         image(playerImg1, x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-        // fill("red");
       }
       else if (grid[y][x] === PLAYER2){
         image(playerImg2, x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-        // fill("red");
       }
     }
   }
