7863e9defe6481ad13a26a4d9bbfe60144de8c7a
Cee-be <7015733@spsd.sk.ca>
2025-11-09 23:19:30 -0600

    Adding images and getting rid of unneeded code

--- messing with c in grid/sketch.js
+++ messing with c in grid/sketch.js
@@ -3,47 +3,63 @@
 //
 //images - https://github.com/hadigghazi/FireBoy-and-WaterGirl/commits?author=DawoudTormos
 
-const CELL_SIZE = 100;
-const OPEN_TILE = 0;
-const IMPOSSIBLE = 1;
+const CELL_SIZE = 50;
+const WALL = 8;
+const WALL_BTN = 9;
 const WATER = 3;
 const LAVA = 2;
-const DOOR = 4;
-const PLAYER = 9;
-const PLAYER2 = 8;
-let grid;
-let rows;
-let cols;
-let thePlayer = {
-  x: 0,
-  y: 3,
+const GREEN = 4;
+const DOOR_FIRE = 6;
+const DOOR_WATER = 7;
+const PLATFORM = 5;
+const EMPTY = 0;
+
+let grid = [
+  [6, 7, 0, 0, 0, 0, 0, 0, 0, 0],
+  [5, 5, 4, 5, 8, 8, 8, 5, 5, 5],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [5, 5, 5, 5, 5, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 5, 5, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [8, 8, 5, 4, 5, 5, 0, 9, 0, 0],
+  [0, 0, 0, 0, 0, 5, 5, 5, 5, 3],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [2, 5, 5, 5, 5, 0, 0, 9, 0, 0],
+  [0, 0, 0, 0, 5, 5, 5, 5, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 5, 5],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [5, 5, 5, 5, 3, 2, 5, 5, 5, 5],
+];
+
+let rows = 15;
+let cols = 10;
+let fireboy = {
+  x: 35,
+  y: 700,
+  w: 40,
+  h: 40,
+  vy: 0,
+  onGround: false
 };
-let thePlayer2 = {
+
+let watergirl = {
   x: 0,
-  y: 2,
+  y: 700,
+  w: 40,
+  h: 40,
+  vy: 0,
+  onGround: false
 };
+
 let playerImg1;
 let playerImg2;
-
-function setup() {
-  createCanvas(windowWidth * 0.9, windowHeight * 0.9);
-  cols = Math.floor(width/CELL_SIZE);
-  rows = Math.floor(height/CELL_SIZE);
-  grid = generateRandomGrid(cols, rows);
-
-  //add player to grid
-  grid[thePlayer.y][thePlayer.x] = PLAYER;
-  grid[thePlayer2.y][thePlayer2.x] = PLAYER2;
-
-  //resizing image
-  playerImg1.resize(300, 500);
-  playerImg2.resize(300, 500);
-}
-
-function draw() {
-  background("beige");
-  displayGrid();
-}
+let tile;
+let waterpit;
+let firepit;
+let greenpit;
+let platformHeight;
+let platformWidth;
 
 function preload(){
   playerImg1 = loadImage("fireboy.png");
@@ -51,154 +67,116 @@ function preload(){
 
   gameBackground = loadImage("Ground.png");
 
-  door = loadImage("door.png");
+  waterdoor = loadImage("watergirl_door.png");
+  firedoor = loadImage("fireboy_door.png");
+  tile = loadImage("tiles.png");
+
+  firepit = loadImage("firepit.png");
+  waterpit = loadImage("waterpit.png");
+  greenpit = loadImage("greenpit.png");
+
+  wall = loadImage("wall.png");
+  wall_btn = loadImage("wallBtn.png");
 }
 
-// function toggleCell(x, y) {
-//   //make sure the cell you're toggling actually exists!
-//   if (x >= 0 && x < cols && y >= 0 && y < rows) {
-//     if (grid[y][x] === OPEN_TILE) {
-//       grid[y][x] = IMPOSSIBLE;
-//     }
-//     else if (grid[y][x] === IMPOSSIBLE) {
-//       grid[y][x] = OPEN_TILE;
-//     }
-//   }
-// }
+function setup() {
+  createCanvas(cols * CELL_SIZE, rows * CELL_SIZE);
+
+  let aspectRatio = tile.width / tile.height;
+  platformWidth = 115;
+  platformHeight = platformWidth / aspectRatio;
+}
+
+function draw() {
+  image(gameBackground, 0, 0, width, height);
+  displayGrid();
+  
+  updatePlayer(fireboy);
+  updatePlayer(watergirl);
+
+  //players
+  image(playerImg1, fireboy.x, fireboy.y, fireboy.w, fireboy.h);
+  image(playerImg2, watergirl.x, watergirl.y, watergirl.w, watergirl.h);
+}
+
+function updatePlayer(p){
+  // gravity
+  p.vy += 0.5;
+  p.y += p.vy;
+
+  //collision
+  let gridX = Math.floor(p.x / CELL_SIZE);
+  let gridY = Math.floor((p.y + p.h)/ CELL_SIZE);
+
+  if (grid[gridY] && grid[gridY][gridX] === PLATFORM){
+    p.vy = 0;
+    p.onGround = true;
+    p.y = gridY * CELL_SIZE - p.h;
+  } 
+  else {
+    p.onGround = false;
+  }
+}
 
 function keyPressed() {
   //fireboy movement
-  if (key === "w"){
-    movePlayer(thePlayer, thePlayer.x, thePlayer.y - 1);
+  if (key === "w" && fireboy.onGround){
+    fireboy.vy = -10;
+    fireboy.onGround = false;
   }
   else if (key === "d"){
-    movePlayer(thePlayer, thePlayer.x + 1, thePlayer.y);
+    fireboy.x += 20;
   }
   else if (key === "a"){
-    movePlayer(thePlayer, thePlayer.x - 1, thePlayer.y);
+    fireboy.x -= 20;;
   }
 
   //watergirl movement
-  if (keyCode === UP_ARROW){
-    movePlayer2(thePlayer2, thePlayer2.x, thePlayer2.y - 1);
+  if (keyCode === UP_ARROW && watergirl.onGround){
+    watergirl.vy = -10;
+    watergirl.onGround = false;
   }
   else if (keyCode ===RIGHT_ARROW){
-    movePlayer2(thePlayer2, thePlayer2.x + 1, thePlayer2.y);
+    watergirl.x += 20;
   }
   else if (keyCode === LEFT_ARROW){
-    movePlayer2(thePlayer2, thePlayer2.x - 1, thePlayer2.y);
-  }
-}
-
-//moving player1(fireboy)
-function movePlayer(player, newX, newY){
-  if (newX < 0 || newX >= cols || newY < 0 || newY >= rows){
-    return;
-  }
-
-  if (grid[newY][newX] === OPEN_TILE) {
-    grid[player.y][player.x] = OPEN_TILE;
-
-    //moving player locat
-    thePlayer.x = newX;
-    thePlayer.y = newY;
-
-  
-    //mark new spot
-    if (player === thePlayer){
-      grid[player.y][player.x] = PLAYER;
-    } 
-    else {
-      grid[player.y][player.x] = PLAYER2;
-    }
-  }
-}
-
-
-//moving player2(watergirl)
-function movePlayer2(player2, newX, newY){
-  if (newX < 0 || newX >= cols || newY < 0 || newY >= rows){
-    return;
-  }
-
-  if (grid[newY][newX] === OPEN_TILE) {
-    grid[player2.y][player2.x] = OPEN_TILE;
-
-    //moving player locat
-    thePlayer2.x = newX;
-    thePlayer2.y = newY;
-
-  
-    //mark new spot
-    if (player2 === thePlayer2){
-      grid[player2.y][player2.x] = PLAYER2;
-    } 
-    else {
-      grid[player.y][player.x] = PLAYER;
-    }
+    watergirl.x -= 20;
   }
 }
 
 function displayGrid() {
   image (gameBackground, 0, 0, width, height);
+
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
       let cell = grid[y][x];
       let xpos = x * CELL_SIZE;
       let ypos = y * CELL_SIZE; 
 
-      if (cell === OPEN_TILE){
-        fill("beige");
-      }
-      else if (cell === IMPOSSIBLE){
-        fill("black");
+      if (cell === PLATFORM){
+        image(tile, xpos, ypos, platformWidth, platformHeight);
       }
       else if (cell === LAVA){
-        fill("red");
+        image(firepit, xpos, ypos, CELL_SIZE, CELL_SIZE);
       }
       else if (cell === WATER){
-        fill("blue");
+        image(waterpit, xpos, ypos, CELL_SIZE, CELL_SIZE);
       }
-      else if (cell === DOOR){
-        fill("green");
+      else if (cell === GREEN){
+        image(greenpit, xpos, ypos, CELL_SIZE, CELL_SIZE);
       }
-      rect(xpos, ypos, CELL_SIZE, CELL_SIZE);
-
-      if (cell === PLAYER){
-        image(playerImg1, xpos, ypos, CELL_SIZE, CELL_SIZE);
+      else if (cell === WALL){
+        image(wall, xpos, ypos, CELL_SIZE, CELL_SIZE);
       }
-      else if (cell === PLAYER2){
-        image(playerImg2, xpos, ypos, CELL_SIZE, CELL_SIZE);
+      else if (cell === WALL_BTN){
+        image(wall_btn, xpos, ypos, CELL_SIZE, CELL_SIZE);
       }
-    }
-  }
-}
-
-function generateRandomGrid(cols, rows) {
-  let grid = [];
-  for (let y = 0; y < rows; y++) {
-    grid[y] = [];
-    for (let x = 0; x < cols; x++) {
-      let r = random(1);
-      //pick 0 or 1 randomly
-      if (random(100) < 50) {
-        grid[y].push(OPEN_TILE);
+      else if (cell === DOOR_WATER){
+        image(waterdoor, xpos, ypos, CELL_SIZE, CELL_SIZE);
       }
-      else {
-        grid[y].push(IMPOSSIBLE);
+      else if (cell === DOOR_FIRE){
+        image(firedoor, xpos, ypos, CELL_SIZE, CELL_SIZE);
       }
     }
   }
-  return grid;
-}
-
-function generateEmptyGrid(cols, rows) {
-  let newGrid = [];
-  for (let y = 0; y < rows; y++) {
-    newGrid.push([]);
-    for (let x = 0; x < cols; x++) {
-      newGrid[y].push(0);
-    }
-  }
-  return newGrid;
 }
